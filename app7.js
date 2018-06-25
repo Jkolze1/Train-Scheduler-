423465fe3ef57cca4f8374975be223f5de538960
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-06-25 16:20:28 -0500

    Add files via upload

--- app7.js
+++ app7.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    var config = {
     apiKey: "AIzaSyC0gX6exBphdv_gvMPbHakI3IcmHlXOFis",
     authDomain: "train-scheduler-d35f9.firebaseapp.com",
-    databaseURL: "https://train-scheduler-d35f9.firebaseio.com",
+    databaseURL: "https://train-scheduler-d35f9.firebaseio.com/",
     projectId: "train-scheduler-d35f9",
     storageBucket: "train-scheduler-d35f9.appspot.com",
     messagingSenderId: "562161257539"
@@ -11,13 +11,15 @@ $(document).ready(function(){
     
 firebase.initializeApp(config);
 var database = firebase.database();
+
 // Capture button click
 $("#submit").on("click", function(event) { 
+  event.preventDefault();
 
-// User input stuff
-     var trainName=$("name-input").val().trim();
+  // User input stuff
+     var trainName=$("name-input").val();
      var destination = $("#destination-input").val().trim();
-     var first = $("#first-input").val().trim();
+     var first = $("#first-input").val();
      var tfrequency = $("#tfrequency-input").val(); //.trim ?
     
 // Push to database
@@ -29,12 +31,13 @@ $("#submit").on("click", function(event) {
         timeAdded: firebase.database.ServerValue.TIMESTAMP
       });
 
-// Refresh NA
+// Refresh 
 $("input").val('');
 return false;
 });
 
 // On click child function
+var dataRef = firebase.database();
   dataRef.ref().on("child_added", function(childSnapshot) { 
     var trainName= childSnapshot.val().name;
     var traindestination = childSnapshot.val().destination;
@@ -50,6 +53,7 @@ return false;
     var tRemainder = diffTime % tFrequency;
     var tMinutesTillTrain = tFrequency - tRemainder;
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    
 // Logs
     console.log(firstTimeConverted);
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
@@ -57,8 +61,10 @@ return false;
     console.log(tRemainder);
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    console.log(firebase);
+    
     // Append to table
-    $("#train-table").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + tFrequency + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+    $("#train-table").append("<tr><td>" + trainName + "</td><td>" + traindestination + "</td><td>" + tFrequency + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
   });
 });
 
