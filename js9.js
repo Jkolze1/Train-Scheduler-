d68a0892f764badc96e1eb5533be084a255855d9
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-06-27 23:45:52 -0500

    Add files via upload

---
+++ js9.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+	// 1. Link to Firebase
+	var trainData = new Firebase("https://train-scheduler-d35f9.firebaseio.com/");
+
+	// 2. Button for adding Trains
+	$("#addTrain").on("click", function(){
+
+		// Grabs user input and assign to variables
+		var trainName = $("#trainNameInput").val().trim();
+		var destination = $("#destinationInput").val().trim();
+		var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
+		var frequencyInput = $("#frequencyInput").val().trim();
+		
+
+		// Test for variables entered
+		console.log(trainName);
+		console.log(destination);
+		console.log(trainTimeInput);
+		console.log(frequencyInput);
+
+		// Creates local "temporary" object for holding train data
+		// Will push this to firebase
+		var newTrain = {
+			name:  trainName,
+			destination: destination,
+			trainTime: trainTimeInput,
+			frequency: frequencyInput,
+		}
+
+		// pushing trainInfo to Firebase
+		trainData.push(newTrain);
+
+		// clears text-boxes upon submit
+		$("#trainNameInput").val("");
+		$("#destinationInput").val("");
+		$("#trainInput").val("");
+		$("#frequencyInput").val("");
+
+		// Prevents page from refreshing
+		return false;
+	});
+
+	trainData.on("child_added", function(childSnapshot, prevChildKey){
+		
+		// Assign firebase variables to snapshots.
+		var firebaseName = childSnapshot.val().name;
+		var firebaseDestination = childSnapshot.val().destination;
+		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
+		var firebaseFrequency = childSnapshot.val().frequency;
+		// Math
+		var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
+		var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
+		var minutes = firebaseFrequency - timeRemainder;
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+		
+		// Console logs correct times and info
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+        console.log(childSnapshot.val());
+        
+		// Append train info to table on page
+		$("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" +  + firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+
+	});
+});
