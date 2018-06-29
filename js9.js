caa8dc5035f2a1be8cd6065a70480debe1ec443f
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-06-29 00:37:10 -0500

    Add files via upload

--- js9.js
+++ js9.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
 		var destination = $("#destinationInput").val().trim();
 		var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
 		var frequencyInput = $("#frequencyInput").val().trim();
-		// Test for variables entered
+		
+		// Variable console log
 		console.log(trainName);
 		console.log(destination);
 		console.log(trainTimeInput);
@@ -41,16 +42,7 @@ $(document).ready(function(){
 		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
 		var firebaseFrequency = childSnapshot.val().frequency;
 	
-		
-		// Console logs correct times and info
-		console.log(minutes);
-		console.log(nextTrainArrival);
-		console.log(moment().format("hh:mm A"));
-		console.log(nextTrainArrival);
-		console.log(moment().format("X"));
-		console.log(childSnapshot.val());
-		
-       // Math
+	// Math
 		var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
 		var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
 		var minutes = firebaseFrequency - timeRemainder;
@@ -59,5 +51,14 @@ $(document).ready(function(){
 		// Append train info to table on page
 		$("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" +  + firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
 
+
+			
+		// Console logs correct times and info
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+		console.log(childSnapshot.val());
 	});
 });
